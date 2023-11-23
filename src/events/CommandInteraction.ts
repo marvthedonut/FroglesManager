827b0f754b86cb5492dbbcbfb3690cf8e99b6ee5
marvthedonut <61897082+marvthedonut@users.noreply.github.com>
2023-11-23 13:43:13 -0800

    Log command usages

--- src/events/CommandInteraction.ts
+++ src/events/CommandInteraction.ts
@@ -1,12 +1,13 @@
-import { CacheType, Events, Interaction, CommandInteraction } from "discord.js";
-import { Event } from "./Event.js";
+import { CacheType, CommandInteraction, Events, Interaction } from "discord.js";
 import CommandManager from "../commands/CommandManager.js";
+import { Event } from "./Event.js";
 
 export default class _CommandInteraction implements Event {
     public id: string = "command_interaction";
     public type: string = Events.InteractionCreate;
     public once = (interaction: Interaction) => {
         if (interaction.isCommand()) {
+            console.log(`${interaction.user.tag} just used ${interaction.commandName}.`);
             CommandManager.callCommand(interaction as CommandInteraction<CacheType>);
         }
     };
